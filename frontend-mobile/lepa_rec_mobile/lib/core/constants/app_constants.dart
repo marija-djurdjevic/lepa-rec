68888381c65066a33a3f703f294114bc36a974a3
Marija Djurdjevic <masadjurdjevic02@gmail.com>
2026-02-18 14:19:00 +0100

    fix: move mobile app to separate repo

--- frontend-mobile/lepa_rec_mobile/lib/core/constants/app_constants.dart
+++
@@ -1,4 +0,0 @@
-class AppConstants {
-  static const String appName = 'Lepa reč';
-  static const String apiBaseUrl = 'http://127.0.0.1:5202/api';
-}
--- frontend-mobile/lepa_rec_mobile/lib/features/auth/data/models/google_login_request.dart
+++
@@ -1,7 +0,0 @@
-class GoogleLoginRequest {
-  final String idToken;
-
-  GoogleLoginRequest({required this.idToken});
-
-  Map<String, dynamic> toJson() => {'idToken': idToken};
-}
