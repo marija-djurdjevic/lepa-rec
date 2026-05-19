d246a5823567c41e365cbe3e2a1ba2165c3954ae
Marija Djurdjevic <masadjurdjevic02@gmail.com>
2026-05-19 03:46:24 +0200

    Mark AddGrowthMessageSkillId with EF migration attributes

--- Practice/Infrastructure/Migrations/20260519120000_AddGrowthMessageSkillId.cs
+++ Practice/Infrastructure/Migrations/20260519120000_AddGrowthMessageSkillId.cs
@@ -1,4 +1,5 @@
 using System;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
@@ -6,6 +7,8 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace AngularNetBase.Practice.Infrastructure.Migrations
 {
     /// <inheritdoc />
+    [DbContext(typeof(PracticeContext))]
+    [Migration("20260519120000_AddGrowthMessageSkillId")]
     public partial class AddGrowthMessageSkillId : Migration
     {
         /// <inheritdoc />
